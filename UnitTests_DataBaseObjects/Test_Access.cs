bca7823fe8c8a962051da34da78098a8f35c4e83
RFBomb <robbery525@gmail.com>
2023-01-10 15:19:16 -0500

    Unit Tests objects

---
+++ UnitTests_DataBaseObjects/Test_Access.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SqlKata.Compilers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using RFBCodeWorks.SqlKata.MsOfficeCompilers;
+
+namespace AccessTests
+{
+    [TestClass]
+    public class Test_Access
+    {
+        // First condition is the is the ANSI-89 which needs to convert to ANSI-92, second condition is the equivalent ANSI-92 search term
+        [DataRow("Search*Term[*]", "Search%Term*")]
+        [DataRow("Search*Term%", "Search%Term[%]")]
+        [DataRow("*#*", "%[0-9]%")]
+        [DataRow("H??p", "H__p")]
+        [DataRow("H?[?]p", "H_?p")]
+        [DataRow("[!3-9]", "[^3-9]")]
+        [TestMethod]
+        public void SanitzeAnsi89ToAnsi92(string value, string expected)
+        {
+            Assert.AreEqual(expected, MSAccessCompiler.SanitizeWildCards(value));
+        }
+    }
+}
